7bdbfd44d9a888395855281af9e240d62401859a
lin <vsnetlin@163.com>
2017-11-16 20:06:59 +0800

    Add find bluetooth device code.

--- app/src/main/java/nhlcgz/com/smartwallet/DeviceListActivity.java
+++ app/src/main/java/nhlcgz/com/smartwallet/DeviceListActivity.java
@@ -1,13 +1,66 @@
 package nhlcgz.com.smartwallet;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
 
 public class DeviceListActivity extends AppCompatActivity {
 
+    private BluetoothAdapter myBluetooth = null;
+    private Set<BluetoothDevice> pairedDevices;
+    ListView deviceList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_device_list);
+        deviceList=(ListView)findViewById(R.id.device_list);
+        checkBluetooth();
+        showDeviceList();
+    }
+
+    private void showDeviceList() {
+        pairedDevices = myBluetooth.getBondedDevices();
+        ArrayList list = new ArrayList();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice bt : pairedDevices) {
+                list.add(bt.getName() + "\n" + bt.getAddress());
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "找不到可以配对的设备……", Toast.LENGTH_LONG).show();
+        }
+        final ArrayAdapter adapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,list);
+        deviceList.setAdapter(adapter);
+        deviceList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String info = ((TextView) view).getText().toString();
+                String address = info.substring(info.length() - 17);
+                // Make an intent to start next MainActivity.
+
+            }
+        });
+    }
+    private void checkBluetooth(){
+        myBluetooth=BluetoothAdapter.getDefaultAdapter();
+        if(myBluetooth==null) {
+            Toast.makeText(getApplicationContext(), "蓝牙设备不可用", Toast.LENGTH_LONG).show();
+        }else{
+            if(!myBluetooth.isEnabled()){
+                Intent turnBTon = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(turnBTon,1);
+            }
+        }
     }
 }
--- app/src/main/java/nhlcgz/com/smartwallet/MainActivity.java
+++ app/src/main/java/nhlcgz/com/smartwallet/MainActivity.java
@@ -1,13 +1,27 @@
 package nhlcgz.com.smartwallet;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bSetting;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bSetting=(Button) findViewById(R.id.bSetting);
+
+        bSetting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,DeviceListActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
